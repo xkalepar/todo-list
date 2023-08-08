2e11e528c94ccc100ee179560274444623402dfd
xkalepar <132944315+xkalepar@users.noreply.github.com>
2023-08-08 02:50:11 +0200

    types

--- public/js/index.js
+++ public/js/index.js
@@ -2,41 +2,43 @@
 const root = document.getElementById("root");
 const postForm = document.getElementById("form-1");
 const localElements = window.localStorage;
-// postForm.onclick = (e) => {
-//   e.preventDefault();
-//   console.log("form");
-// };
-// console.log(root);
 const loop = () => {
     let array = [];
-    for (let i = 0; i <= localElements.length; i++) {
-        array.push(localElements.key(i));
+    for (let i = 0; i < localElements.length; i++) {
+        const key = localElements.key(i);
+        array.unshift(key);
     }
-    return array;
+    const filteredArray = array.filter((todo) => todo.startsWith("todo"));
+    console.log(filteredArray);
+    return filteredArray;
 };
 const allElements = loop();
+// console.log(allElements);
 window.onload = () => {
-    presentTodos(allElements, "todo-container");
+    if (allElements.length > 0) {
+        presentTodos(allElements, "todo-container");
+    }
+    return;
 };
 accessDom("form-1-input", "form-1-submit");
 function accessDom(childId, submitId) {
     const input = document.getElementById(childId);
     const submit = document.getElementById(submitId);
-    let array = [];
     submit.onclick = (e) => {
-        // e.preventDefault();
-        window.localStorage.setItem(input.value, input.value);
-        array.push(input.value);
-        presentTodos(array, "todo-container");
+        window.localStorage.setItem("todo~" + input.value, input.value);
+        loop().unshift("todo~" + input.value);
+        presentTodos(loop(), "todo-container");
     };
 }
 const presentTodos = (array, containerId) => {
     var _a;
-    for (let i = 0; i <= array.length; i++) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = "";
+    for (let i = 0; i < array.length; i++) {
         const div = document.createElement("div");
         div.className =
-            "border border-solid border-black rounded-xl text-black px-4 py-1 min-h-[35px] w-full";
-        div.innerText = array[i];
+            "border border-solid border-black rounded-xl text-black px-4 py-1 min-h-[35px] w-full mt-2";
+        div.innerText = array[i].split("~")[1];
         (_a = document.getElementById(containerId)) === null || _a === void 0 ? void 0 : _a.append(div);
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,47 @@
-interface Todo {
-  text: string;
-}
-
 const root = document.getElementById("root") as HTMLDivElement;
 const postForm = document.getElementById("form-1") as HTMLFormElement;
 const localElements = window.localStorage;
 
-// postForm.onclick = (e) => {
-//   e.preventDefault();
-//   console.log("form");
-// };
-// console.log(root);
-
 const loop = (): string[] => {
   let array: string[] = [];
-  for (let i: number = 0; i <= localElements.length; i++) {
-    array.push(localElements.key(i) as string);
+  for (let i: number = 0; i < localElements.length; i++) {
+    const key = localElements.key(i) as string;
+    array.unshift(key);
   }
-  return array;
+  const filteredArray = array.filter((todo) => todo.startsWith("todo"));
+  console.log(filteredArray);
+  return filteredArray;
 };
 
 const allElements: string[] = loop();
+// console.log(allElements);
 
 window.onload = () => {
-  presentTodos(allElements, "todo-container");
+  if (allElements.length > 0) {
+    presentTodos(allElements, "todo-container");
+  }
+  return;
 };
 
 accessDom("form-1-input", "form-1-submit");
 function accessDom(childId: string, submitId: string): void {
   const input = document.getElementById(childId) as HTMLInputElement;
   const submit = document.getElementById(submitId) as HTMLInputElement;
-  let array: string[] = [];
   submit.onclick = (e) => {
-    // e.preventDefault();
-    window.localStorage.setItem(input.value, input.value);
-    array.push(input.value);
-    presentTodos(array, "todo-container");
+    window.localStorage.setItem("todo~" + input.value, input.value);
+    loop().unshift("todo~" + input.value);
+    presentTodos(loop(), "todo-container");
   };
 }
 
 const presentTodos = (array: string[], containerId: string): void => {
-  for (let i: number = 0; i <= array.length; i++) {
+  const container = document.getElementById(containerId) as HTMLDivElement;
+  container.innerHTML = "";
+  for (let i: number = 0; i < array.length; i++) {
     const div = document.createElement("div");
     div.className =
-      "border border-solid border-black rounded-xl text-black px-4 py-1 min-h-[35px] w-full";
-    div.innerText = array[i];
+      "border border-solid border-black rounded-xl text-black px-4 py-1 min-h-[35px] w-full mt-2";
+    div.innerText = array[i].split("~")[1];
     document.getElementById(containerId)?.append(div);
   }
 };
